d48e12ca9585f78352539a446e3c131afa790636
Markus Höcherl <mhoecherl@freenet.de>
2015-02-26 10:40:16 +0100

    CRM_Client

---
+++ src/CRM_Client_Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MarkusH
+ */
+public class CRM_Client_Main {
+    
+    public static void main(String args[]) {
+        CRM_Client_Methoden client = new CRM_Client_Methoden();
+        client.connectServer();
+        
+        
+    }
+    
+}
